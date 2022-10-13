18386e6b5de6480b75d139e9b330aded47e1ba2c
Mcshelton96 <mcshelton96@gmail.com>
2022-10-12 21:36:00 -0500

    1-4 completed

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,38 @@
-export const TicketList = () => {
-    return <h2>List of Tickets</h2>
-}
+import { useEffect, useState } from "react"
 
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/serviceTickets`)
+            .then(response => response.json())
+            .then((ticketArray) => {
+                setTickets(ticketArray)
+            })
+        // could also look like below, above uses .then, below uses async/await
+            // const fetchData = async () => {
+            //     const response = await fetch(`http://localhost:8088/serviceTickets`)
+            //     const ticketArray = await response.json()
+            //     setTickets(ticketArray)
+            // }
+            // fetchData()
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+    return <>
+      <h2>List of Tickets</h2>
+      <article className="tickets">
+        {
+          tickets.map(
+            (ticket) => {
+              return <section key={ticket.id} className="ticket">
+                <header>{ticket.description}</header>
+                <footer>Emergency: {ticket.emergency ? "❗" : "No" }</footer>
+              </section>
+            }
+          )
+        }
+      </article>
+    </>
+}
